664342113eb9317182ebfe3f48d272e552662a97
Prabath5362 <nethminaprabath@gmail.com>
2025-08-23 16:57:02 +0530

    addProduct method finished

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,35 @@
-export function addProduct(req,res){
-    if(req.user == null){
+import Product from "../models/product.js";
+
+export function addProduct(req, res) {
+    if (req.user == null) {
         res.status(401).json({
             message: "Login first and try again"
         })
         return;
     }
 
-    if(req.user.role != "admin"){
+    if (req.user.role != "admin") {
         res.status(401).json({
             message: "You are not authorized to perform this task"
         })
         return;
     }
 
-    if(req.user.role == "admin"){
-         res.json({
-            message: "Product add success ✅"
+    if (req.user.role == "admin") {
+        const productData = req.body;
+
+        const product = Product(productData);
+
+        product.save().then(() => {
+            res.json({
+                message: "Product add success ✅"
+            })
+        }).catch((e)=>{
+             res.json({
+                message: "Product couldn't add error: "+e
+            })
         })
+
+
     }
 }
